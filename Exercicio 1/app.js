741ce6be4be9dde92c6c1ffd8363c49498313003
Maria Heloisa <mariaheloisa@pontogovsistemas.com.br>
2025-08-07 10:57:37 -0300

    feat: adicioando botoes subtrair, multiplicar e dividir

--- Exercicio 1/app.js
+++ Exercicio 1/app.js
@@ -1,10 +1,16 @@
 let campoValor1 = document.getElementById("valor1")
 let campoValor2 = document.getElementById("valor2")
 let botaoSomar = document.getElementById("soma")
+let botaoSubtrair = document.getElementById("subtrair")
+let botaoMultiplicar = document.getElementById("multiplicar")
+let botaoDividir = document.getElementById("dividir")
 let valorResultado = document.getElementById("resultado")
 let resultado = 0;
 
 botaoSomar.addEventListener("click", somar)
+botaoSubtrair.addEventListener("click",subtrair)
+botaoMultiplicar.addEventListener("click",multiplicar)
+botaoDividir.addEventListener("click",dividir)
 
 function somar(){
     if(campoValor1.value != "" && campoValor2.value != "" ){
@@ -13,8 +19,44 @@ function somar(){
         resultado = valor1 + valor2
         valorResultado.innerHTML = resultado
     } else{
-        valorResultado.innerHTML = "ERRO! É necessário informar os valores"
+        aviso()
     }
+}
+
+function subtrair(){
+    if(campoValor1.value != "" && campoValor2.value != ""){
+        let valor1 = Number(campoValor1.value)
+        let valor2 = Number(campoValor2.value)
+        resultado = valor1 - valor2
+        valorResultado.innerHTML = resultado
+    } else{
+        aviso()
+    }
+}
+
+function multiplicar(){
+    if(campoValor1.value != "" && campoValor2.value != ""){
+        let valor1 = Number(campoValor1.value)
+        let valor2 = Number(campoValor2.value)
+        resultado = valor1 * valor2
+        valorResultado.innerHTML = resultado
+    } else{
+        aviso()
+    }
+}
+
+function dividir(){
+    if(campoValor1.value != "" && campoValor2.value != ""){
+        let valor1 = Number(campoValor1.value)
+        let valor2 = Number(campoValor2.value)
+        resultado = valor1 / valor2
+        valorResultado.innerHTML = resultado
+    } else{
+        aviso()
+    }
+}
+function aviso(){
+    valorResultado.innerHTML = "ERRO! É necessário informar os valores"
 }
 
 console.log(resultado)
